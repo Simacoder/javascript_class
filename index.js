01ec4214e85b653c0063aec1c5c4f34011e2721a
Simanga <simacoder@hotmail.com>
2024-04-16 16:41:26 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,37 @@
+// the way to declare variables 
+//var
+// let 
+// const: 
+
+//var username = "Simanga"
+//console.log(username);
+
+//let age = 35
+//console.log({age});
+
+// data-types - primitive 
+let username = "Simanga"
+let age = 335
+let isStudent = true
+let surname = null
+
+console.log({username, age, isStudent, surname});
+
+// non-primitve data-types
+
+let listOfstudent = ["Simanga", "Gontse", "Nomcebo","Thabang"]
+console.log({listOfstudent});
+console.log(listOfstudent.length);
+
+// objects non - primitive data-types
+
+let students = {
+    name: "Simanga",
+    surname: "Mchunu",
+    id_number: 881120,
+    age: 35,
+    isStudent: true
+}
+
+console.log(students);
+// 
