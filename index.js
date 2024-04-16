68e02156bbd45ae3bc0d129b802da944169119b4
Simanga <simacoder@hotmail.com>
2024-04-16 17:32:50 +0200

    added logical conditions

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-// work with operators 
+// mixing logical and control flow 
 
-// =,-,/* , %
-// interger addition
-console.log(4 + 3);
-// string concatetinator
-console.log("Hi, there," + " my name is Simanga");
+let num1 = 4
+let num2 = 6
 
-// explicitly 
-console.log(String(4) + String(5));
-console.log("5" + "9");
-// direct conversion 
-console.log(Number("5") + Number("9"));
-
-// implicitly conversion
-console.log(6 + "6" + 6);
-
-let num1 = 100
-let num2 = 50
-let subtract = num1 - num2
-console.log(subtract);
-
-// division 
-let quotien = num1 / num2
-console.log(quotien);
-
-// multiplication 
-
-console.log(num1 * num2);
-
-// logical operators
-
-// &&, ||, ==, !==, ===
-// if statements 
-
-if(5 >= 5){
-    console.log(true);
+if (num1 > num2) {
+    console.log("The first number is high");
 }
 else {
-    console.log(false);
+    console.log("Second number is high");
 }
 
+if ((num1 > num2) && ( 5 < 6)){
+    console.log("Both condition are true");
+}
+else {
+    console.log("not both are met");
+}
